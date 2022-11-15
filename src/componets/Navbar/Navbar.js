3bb62eb5b4584f63061633bba5405e196be892ce
Francisco Roldán <francisco_rol83@hotmail.com>
2022-11-15 15:48:55 -0300

    Agregando un poco de BS

--- src/componets/Navbar/Navbar.js
+++ src/componets/Navbar/Navbar.js
@@ -2,14 +2,19 @@ import './Navbar.css'
 
 const Navbar  = () => {
     return (
-        <nav>
-            <h1 style={{ color: 'red'}}>Prueba de Mi NavBar para React</h1>
-            <botton className='boton'>BOTON</botton>
-            
-            <botton className='boton'>BOTON</botton>
+        <nav class="navbar navbar-dark bg-dark fixed-top">
+            <h1>APOLO COMPANY</h1>
+            <div class="container-fluid">
+                <form class="container-fluid justify-content-start">
+                    <button class="btn btn-sm btn-outline-secondary" type="button">Home</button>
+                    <button class="btn btn-sm btn-outline-secondary" type="button">¿Quienes Somos?</button>
+                    <button class="btn btn-sm btn-outline-secondary" type="button">Acerca de esta App</button>
+                    <button class="btn btn-sm btn-outline-secondary" type="button">Productos</button>
+                </form>
+            </div>
+
         </nav>
+
     )
-
 }
-
 export default Navbar
