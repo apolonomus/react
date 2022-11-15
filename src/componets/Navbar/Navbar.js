e08b4f0d5b14372835fa564918e000e07828b945
Francisco Roldán <francisco_rol83@hotmail.com>
2022-11-15 13:32:00 -0300

    Agregando Navbar

---
+++ src/componets/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import './Navbar.css'
+
+const Navbar  = () => {
+    return (
+        <nav>
+            <h1 style={{ color: 'red'}}>Prueba de Mi NavBar para React</h1>
+            <botton className='boton'>BOTON</botton>
+            
+            <botton className='boton'>BOTON</botton>
+        </nav>
+    )
+
+}
+
+export default Navbar
